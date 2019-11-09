b32d72dd6417c1d88dfdae666b7eb7ac2fe9b84b
Keith Curbelo <keithcurbelo@yahoo.com>
2019-11-09 17:17:04 -0500

    add redirect for invalid paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 
 const App = () => {
@@ -7,6 +7,7 @@ const App = () => {
     <div className="App">
       <Router>
         <Route path="/" component={Home} />
+        <Redirect from="*" to="" />
       </Router>
     </div>
   );

ebf2db8d3f2af2c79fc1d1c7dd7065120acfe784
Keith Curbelo <keithcurbelo@yahoo.com>
2019-11-09 17:43:21 -0500

    create sass style file and import bootstrap 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
+import "./App.scss";
 
 const App = () => {
   return (

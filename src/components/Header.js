4731c6729a5b379d7530265ff7fb3bd7d40921df
Keith Curbelo <keithcurbelo@yahoo.com>
2020-10-25 00:22:07 -0400

    remove phone number, add ids

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,15 @@ import ProfilePic from "../assets/images/profile_pic.jpg";
 import {Row, Col, Image } from "react-bootstrap"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFileAlt, faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const Header = () => {
   return (
     <Row
       id="header-info"
       className="px-4 bg-white"
-      style={{ paddingTop: "70px" }}
+      style={{ paddingTop: "70px"}}
     >
       <Col xs={12} md={4} className="text-center" style={{ height: "180px" }}>
         <Row className="sm-height d-none d-md-block"></Row>
@@ -38,8 +39,8 @@ const Header = () => {
         </Row>
       </Col>
       <Col xs={12} md={8} className="bg-white header-deatils">
-        <Row className="sm-height d-sm-none d-md-block"></Row>
-        <Row>
+        {/* <Row className="sm-height d-sm-none d-md-block" style={{height: '200px'}}></Row> */}
+        <Row id="profile-name">
           <Col xs={12} className="text-center text-md-left">
             <h1 className="text-center text-md-left">
               <span className="media font-weight-light text-muted">Keith</span>
@@ -49,14 +50,14 @@ const Header = () => {
             </h1>
           </Col>
         </Row>
-        <Row className="mb-3 text-center">
+        <Row id="description" className="mb-3 text-center">
           <Col xs={12} md={6} className="font-weight-light text-md-left text-muted">
             <div className="media manuscript">
               Web & Mobile Application Developer
             </div>
           </Col>
-          <Col id="header-icons" xs={12} md={6} className="mt-3 mt-md-0 text-md-right">
-            <span className="mr-3 d-inline-block d-md-block">
+          <Col id="header-icons" xs={12} md={6} className="mt-3 mt-md-0 text-md-right ">
+            <span className="mr-3 d-inline-block d-md-block" style={{height: '50px'}}>
               <a
                 href="https://www.linkedin.com/in/keithcurbelo"
                 target="_blank"
@@ -65,7 +66,7 @@ const Header = () => {
                 <FontAwesomeIcon icon={faLinkedin} size="2x"/>
               </a>
             </span>
-            <span className="mr-3 d-inline-block d-md-block">
+            <span className="mr-3 d-inline-block d-md-block" style={{height: '50px'}}>
               <a
                 href="https://www.facebook.com/keith.curbelo"
                 target="_blank"
@@ -74,7 +75,7 @@ const Header = () => {
                 <FontAwesomeIcon icon={faFacebookF} size="2x"/>
               </a>
             </span>
-            <span className="mr-0 mr-md-3 d-inline-block d-md-block">
+            <span className="mr-0 mr-md-3 d-inline-block d-md-block" style={{height: '50px'}}>
               <a
                 href="https://drive.google.com/file/d/1jgcC__spBqBppKc6cX3PDpK8fS2CN7ud/view?usp=sharing"
                 target="_blank"
@@ -85,21 +86,18 @@ const Header = () => {
             </span>
           </Col>
         </Row>
-        <Row className="md-height d-sm-none d-md-block"></Row>
+        {/* <Row className="d-sm-none d-md-block" style={{height: '200px'}}></Row> */}
         <Row id="header-box" className="border-top text-center pt-3 text-muted">
-          <Col xs={12} md={4} className="mb-3 header-info">
-            {/* <h6 className="text-dark-blue">Location</h6>  */}
-            <i className="fas fa-map-marker-alt fa-1x text-dark-blue"></i>
+          <Col xs={12} md={6} className="mb-3 header-info">
+            <FontAwesomeIcon icon={faMapMarkerAlt} size="1x" className="text-dark-blue mr-3"/>
             <span className="font-weight-light">Miami, FL</span>
           </Col>
-          <Col xs={12} md={4} className="mb-3 header-info">
-            {/* <h6 className="text-dark-blue">Phone</h6>  */}
-            <i className="fas fa-mobile-alt fa-1x text-dark-blue"></i>
+          {/* <Col xs={12} md={4} className="mb-3 header-info">
+            <FontAwesomeIcon icon={faMobile} size="1x" className="text-dark-blue mr-3"/>
             <span className="font-weight-light">786.210.1141</span>
-          </Col>
-          <Col xs={12} md={4} className="mb-3 header-info px-md-2">
-            {/* <h6 className="text-dark-blue">E-Mail</h6> */}
-            <i className="fas fa-envelope fa-1x text-dark-blue"></i>
+          </Col> */}
+          <Col xs={12} md={6} className="mb-3 header-info px-md-2">
+            <FontAwesomeIcon icon={faEnvelope} size="1x" className="text-dark-blue mr-3"/>
             <span className="font-weight-light">contact@keithcurbelo.com</span>
           </Col>
         </Row>

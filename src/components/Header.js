cf3aa3c104c0c9ee2f0eed9c67e9c15b726e8b06
Keith Curbelo <keithcurbelo@yahoo.com>
2020-10-24 23:10:46 -0400

    convert col from class to component

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
           roundedCircle
         />
         <Row className="lg-height d-sm-none d-md-block">
-          <div className="col-11 d-flex align-items-end h-100 float-right">
+          <Col xs={11} className="d-flex align-items-end h-100 float-right">
             <blockquote className="blockquote text-right d-none d-md-block header-info">
               <p className="mb-0 text-muted handwriting">
                 If I had asked people what they wanted, they would have said
@@ -31,31 +31,28 @@ const Header = () => {
                 </small>
               </footer>
             </blockquote>
-          </div>
+          </Col>
         </Row>
       </Col>
       <Col xs={12} md={8} className="bg-white header-deatils">
         <Row className="sm-height d-sm-none d-md-block"></Row>
         <Row>
-          <div className="col-12 text-center text-md-left">
+          <Col xs={12} className="text-center text-md-left">
             <h1 className="text-center text-md-left">
               <span className="media font-weight-light text-muted">Keith</span>
               <span className="media text-uppercase text-theme sharpie">
                 Curbelo
               </span>
             </h1>
-          </div>
+          </Col>
         </Row>
         <Row className="mb-3 text-center">
-          <div className="col-12 col-md-6 font-weight-light text-md-left text-muted">
+          <Col xs={12} md={6} className="font-weight-light text-md-left text-muted">
             <div className="media manuscript">
               Web & Mobile Application Developer
             </div>
-          </div>
-          <div
-            id="header-icons"
-            className="col-12 col-md-6 mt-3 mt-md-0 text-md-right"
-          >
+          </Col>
+          <Col id="header-icons" xs={12} md={6} className="mt-3 mt-md-0 text-md-right">
             <span className="mr-3 d-inline-block d-md-block">
               <a
                 href="https://www.linkedin.com/in/keithcurbelo"
@@ -83,24 +80,20 @@ const Header = () => {
                 <i className="far fa-file-alt fa-2x"></i>
               </a>
             </span>
-          </div>
+          </Col>
         </Row>
         <Row className="md-height d-sm-none d-md-block"></Row>
         <Row id="header-box" className="border-top text-center pt-3 text-muted">
-          <div className="col-12 col-md-4 mb-3 header-info">
+          <Col className="col-12 col-md-4 mb-3 header-info">
             {/* <h6 className="text-dark-blue">Location</h6>  */}
             <i className="fas fa-map-marker-alt fa-1x text-dark-blue"></i>
             <span className="font-weight-light">Miami, FL</span>
-          </div>
-          <div className="col-12 col-md-4 mb-3 header-info">
+          </Col>
+          <Col xs={12} md={4} className="mb-3 header-info">
             {/* <h6 className="text-dark-blue">Phone</h6>  */}
             <i className="fas fa-mobile-alt fa-1x text-dark-blue"></i>
             <span className="font-weight-light">786.210.1141</span>
-          </div>
-          {/* <div className="col-12 col-md-3 mb-3">
-                    <h6 className="">Website</h6>
-                    <span className="font-weight-light">www.keithcurbelo.com</span>
-                </div> */}
+          </Col>
           <Col xs={12} md={4} className="mb-3 header-info px-md-2">
             {/* <h6 className="text-dark-blue">E-Mail</h6> */}
             <i className="fas fa-envelope fa-1x text-dark-blue"></i>

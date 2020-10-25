96fce3cf8ce0d15ed35046dbca3e63ea201553ca
Keith Curbelo <keithcurbelo@yahoo.com>
2020-10-24 23:02:33 -0400

    replace class with className and update divs

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,107 +6,107 @@ const Header = () => {
   return (
     <Row
       id="header-info"
-      class="px-4 bg-white"
+      className="px-4 bg-white"
       style={{ paddingTop: "70px" }}
     >
-      <div class="col-12 col-md-4 text-center" style={{ height: "180px" }}>
-        <Row class="sm-height d-none d-md-block"></Row>
+      <Col xs={12} md={4} className="text-center" style={{ height: "180px" }}>
+        <Row className="sm-height d-none d-md-block"></Row>
         <Image
           src={ProfilePic}
           id="profile-pic"
           alt="profile_pic"
-          class="shadow"
+          className="shadow"
           roundedCircle
         />
-        <div class="row lg-height d-sm-none d-md-block">
-          <div class="col-11 d-flex align-items-end h-100 float-right">
-            <blockquote class="blockquote text-right d-none d-md-block header-info">
-              <p class="mb-0 text-muted handwriting">
+        <Row className="lg-height d-sm-none d-md-block">
+          <div className="col-11 d-flex align-items-end h-100 float-right">
+            <blockquote className="blockquote text-right d-none d-md-block header-info">
+              <p className="mb-0 text-muted handwriting">
                 If I had asked people what they wanted, they would have said
                 faster horses.
               </p>
-              <footer class="blockquote-footer font-italic">
+              <footer className="blockquote-footer font-italic">
                 <small>
                   <cite title="Source Title">Henry Ford</cite>
                 </small>
               </footer>
             </blockquote>
           </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-8 bg-white header-deatils">
-        <div class="row sm-height d-sm-none d-md-block"></div>
-        <div class="row">
-          <div class="col-12 text-center text-md-left">
-            <h1 class="text-center text-md-left">
-              <span class="media font-weight-light text-muted">Keith</span>
-              <span class="media text-uppercase text-theme sharpie">
+        </Row>
+      </Col>
+      <Col xs={12} md={8} className="bg-white header-deatils">
+        <div className="row sm-height d-sm-none d-md-block"></div>
+        <div className="row">
+          <div className="col-12 text-center text-md-left">
+            <h1 className="text-center text-md-left">
+              <span className="media font-weight-light text-muted">Keith</span>
+              <span className="media text-uppercase text-theme sharpie">
                 Curbelo
               </span>
             </h1>
           </div>
         </div>
-        <div class="row mb-3 text-center">
-          <div class="col-12 col-md-6 font-weight-light text-md-left text-muted">
-            <div class="media manuscript">
+        <div className="row mb-3 text-center">
+          <div className="col-12 col-md-6 font-weight-light text-md-left text-muted">
+            <div className="media manuscript">
               Web & Mobile Application Developer
             </div>
           </div>
           <div
             id="header-icons"
-            class="col-12 col-md-6 mt-3 mt-md-0 text-md-right"
+            className="col-12 col-md-6 mt-3 mt-md-0 text-md-right"
           >
-            <span class="mr-3 d-inline-block d-md-block">
+            <span className="mr-3 d-inline-block d-md-block">
               <a
                 href="https://www.linkedin.com/in/keithcurbelo"
                 target="_blank"
-                class="media-icon"
+                className="media-icon"
               >
-                <i class="fab fa-linkedin fa-2x"></i>
+                <i className="fab fa-linkedin fa-2x"></i>
               </a>
             </span>
-            <span class="mr-3 d-inline-block d-md-block">
+            <span className="mr-3 d-inline-block d-md-block">
               <a
                 href="https://www.facebook.com/keith.curbelo"
                 target="_blank"
-                class="media-icon"
+                className="media-icon"
               >
-                <i class="fab fa-facebook fa-2x"></i>
+                <i className="fab fa-facebook fa-2x"></i>
               </a>
             </span>
-            <span class="mr-0 mr-md-3 d-inline-block d-md-block">
+            <span className="mr-0 mr-md-3 d-inline-block d-md-block">
               <a
                 href="https://drive.google.com/file/d/1jgcC__spBqBppKc6cX3PDpK8fS2CN7ud/view?usp=sharing"
                 target="_blank"
-                class="media-icon"
+                className="media-icon"
               >
-                <i class="far fa-file-alt fa-2x"></i>
+                <i className="far fa-file-alt fa-2x"></i>
               </a>
             </span>
           </div>
         </div>
-        <div class="row md-height d-sm-none d-md-block"></div>
-        <div id="header-box" class="row border-top text-center pt-3 text-muted">
-          <div class="col-12 col-md-4 mb-3 header-info">
-            {/* <h6 class="text-dark-blue">Location</h6>  */}
-            <i class="fas fa-map-marker-alt fa-1x text-dark-blue"></i>
-            <span class="font-weight-light">Miami, FL</span>
+        <div className="row md-height d-sm-none d-md-block"></div>
+        <Row id="header-box" className="border-top text-center pt-3 text-muted">
+          <div className="col-12 col-md-4 mb-3 header-info">
+            {/* <h6 className="text-dark-blue">Location</h6>  */}
+            <i className="fas fa-map-marker-alt fa-1x text-dark-blue"></i>
+            <span className="font-weight-light">Miami, FL</span>
           </div>
-          <div class="col-12 col-md-4 mb-3 header-info">
-            {/* <h6 class="text-dark-blue">Phone</h6>  */}
-            <i class="fas fa-mobile-alt fa-1x text-dark-blue"></i>
-            <span class="font-weight-light">786.210.1141</span>
+          <div className="col-12 col-md-4 mb-3 header-info">
+            {/* <h6 className="text-dark-blue">Phone</h6>  */}
+            <i className="fas fa-mobile-alt fa-1x text-dark-blue"></i>
+            <span className="font-weight-light">786.210.1141</span>
           </div>
-          {/* <div class="col-12 col-md-3 mb-3">
-                    <h6 class="">Website</h6>
-                    <span class="font-weight-light">www.keithcurbelo.com</span>
+          {/* <div className="col-12 col-md-3 mb-3">
+                    <h6 className="">Website</h6>
+                    <span className="font-weight-light">www.keithcurbelo.com</span>
                 </div> */}
-          <div class="col-12 col-md-4 mb-3 header-info px-md-2">
-            {/* <h6 class="text-dark-blue">E-Mail</h6> */}
-            <i class="fas fa-envelope fa-1x text-dark-blue"></i>
-            <span class="font-weight-light">contact@keithcurbelo.com</span>
-          </div>
-        </div>
+          <Col xs={12} md={4} className="mb-3 header-info px-md-2">
+            {/* <h6 className="text-dark-blue">E-Mail</h6> */}
+            <i className="fas fa-envelope fa-1x text-dark-blue"></i>
+            <span className="font-weight-light">contact@keithcurbelo.com</span>
+          </Col>
+        </Row>
       </div>
     </Row>
   );

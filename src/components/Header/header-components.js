13d31eeabcd9f4d58a8aa72de8ef4f9fec656832
Keith Curbelo <keithcurbelo@yahoo.com>
2020-10-26 23:00:43 -0400

    create component for links and position

--- src/components/Header/header-components.js
+++ src/components/Header/header-components.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {Row, Col } from "react-bootstrap"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFileAlt} from "@fortawesome/free-solid-svg-icons";
 
-export const Name = ()=>(
+export const Name = () =>(
     <Row id="profile-name">
         <Col xs={12} className="text-center text-md-left">
             <h1 className="text-center text-md-left">
@@ -14,6 +16,15 @@ export const Name = ()=>(
         </Col>
    </Row>
 )
+
+export const Position = () =>(
+    <Col xs={12} md={6} className="font-weight-light text-md-left text-muted">
+        <div className="media manuscript">
+            Web & Mobile Application Developer
+        </div>
+  </Col>
+)
+
 export const MediaIcon = ({ className, icon, link,iconSize, ...props}) => (
     <span {...props} className={"mr-3 d-inline-block d-md-block " + className} style={{height: '50px'}}>
         <a
@@ -25,4 +36,12 @@ export const MediaIcon = ({ className, icon, link,iconSize, ...props}) => (
         <FontAwesomeIcon icon={icon} size={iconSize}/>
         </a>
     </span>
+)
+
+export const Links = ()=> (
+    <Col id="header-icons" xs={12} md={6} className="mt-3 mt-md-0 text-md-right ">
+        <MediaIcon icon={faLinkedin} iconSize="2x" link="https://www.linkedin.com/in/keithcurbelo"/>
+        <MediaIcon icon={faFacebookF} iconSize="2x" link="https://www.facebook.com/keith.curbelo"/>
+        <MediaIcon icon={faFileAlt} iconSize="2x" link="https://drive.google.com/file/d/1jgcC__spBqBppKc6cX3PDpK8fS2CN7ud/view?usp=sharing"/>
+    </Col>
 )

a673fbd20b6971247c7f5d15844fae5d29334ca8
Keith Curbelo <keithcurbelo@yahoo.com>
2020-10-26 22:54:17 -0400

    create name component

--- src/components/Header/header-components.js
+++ src/components/Header/header-components.js
@@ -1,5 +1,19 @@
 import React from "react";
+import {Row, Col } from "react-bootstrap"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export const Name = ()=>(
+    <Row id="profile-name">
+        <Col xs={12} className="text-center text-md-left">
+            <h1 className="text-center text-md-left">
+                <span className="media font-weight-light text-muted">Keith</span>
+                <span className="media text-uppercase text-theme sharpie">
+                Curbelo
+                </span>
+            </h1>
+        </Col>
+   </Row>
+)
 export const MediaIcon = ({ className, icon, link,iconSize, ...props}) => (
     <span {...props} className={"mr-3 d-inline-block d-md-block " + className} style={{height: '50px'}}>
         <a

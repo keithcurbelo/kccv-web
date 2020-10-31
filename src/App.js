2a0c6926c7ea7eb1cb3f92f7ec7fd1311b191459
Keith Curbelo <keithcurbelo@yahoo.com>
2020-10-31 17:55:54 -0400

    import icons file into app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.scss";
+import "./assets/icons";
 
 const App = () => {
   return (

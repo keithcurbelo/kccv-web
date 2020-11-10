41ab2a834b11903e1747b4f51d374d868ae04a49
Keith Curbelo <keithcurbelo@yahoo.com>
2020-11-09 22:27:04 -0500

    add email component to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,33 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Row, Col } from "react-bootstrap";
+import { ContactEmail } from "./footer-components";
 
 export default () => {
   return (
     <footer id="footer" className="w-100">
       {/* <!------- Contact ------> */}
       <Row className="border-top bg-dark-blue text-light">
-        {/* <Col xs={12} md={4} className="px-4 py-3 text-md-right sm-height">
-          <h3>Contact</h3>
-          <span className="font-weight-light"> </span>
-        </Col> */}
         <Col className="pt-0 pt-md-3 px-4">
-          <Row className="row text-center text-theme">
-            {/* <div className="col-12 col-md-6 mb-3">
-              <p className="font-weight-light">
-                <FontAwesomeIcon icon={["fas", "mobile-alt"]} size="1x" />{" "}
-                786.210.1141
-              </p>
-            </div> */}
-
-            <Col>
-              {/* <!-- <h6 className="">E-Mail</h6> --> */}
-              <FontAwesomeIcon icon={["fas", "envelope"]} size="1x" />
-              <span className="font-weight-light ml-2">
-                contact@keithcurbelo.com
-              </span>
-            </Col>
-          </Row>
+          <ContactEmail />
           <Row className="text-center my-2">
             <Col>
               <a
